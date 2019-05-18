915c2f25e775d5afb0219159471c3cc00a79c3b6
trrttrtrsrfewrwed <timurruskiy2@mail.ru>
2019-05-18 19:09:59 +0300

    added instructions for use

--- client.py
+++ client.py
@@ -5,9 +5,17 @@ def get_all_functions(_class):
     return [key for key in _class.__dict__ if not key.startswith("private")]
 
 
-opening_string = "Glad to see you in the 'MEAL SCHEDULE'. We will notify you when to buy and eat products." + \
+opening_string = "Glad to see you in the 'MEAL SCHEDULE'. We will notify you when to buy and eat products." \
                  "\nAll you need to do is to choose schedule or make your own and update products you have"
+instructions_for_use = "In order to use the program you have to choose schedule" \
+                       "\n TODO: \n - write 'update_schedule' without params, you will see the following sentence: " \
+                       "\n 'Do you want to change your schedule/choose new one/go back to menu? '1'/'2'/'3':' " \
+                       "\n - write '2' and follow appeared instructions." \
+                       "\nAlso you need to fill list of products with products you have now" \
+                       "\n TODO: \n - write 'update_product_list' without params and follow appeared instructions." \
+                       "\nAfter these two steps you can completely use all other options."
 help_string = "Options:" +\
+              "\n 'instructions' -shows instructions for using programm" + \
               "\n 'exit' -ends the programm" + \
               "\n 'show' -shows your schedule if it exists" + \
               "\n 'notify' -notifies you to eat and buy products if you have schedule" + \
@@ -31,6 +39,7 @@ if __name__ == '__main__':
     functions = get_all_functions(client_methods.Client)
     end_of_prog = False
     print(opening_string)
+    print(instructions_for_use)
     print(help_string)
     while not end_of_prog:
         input_ = input().split()
@@ -45,6 +54,8 @@ if __name__ == '__main__':
                           "Write 'help' to revise request type.", sep='\n')
             elif function == 'help':
                 print(help_string)
+            elif function == 'instructions':
+                print(instructions_for_use)
             elif function == 'exit':
                 client.private_set_notifier()
                 end_of_prog = True
